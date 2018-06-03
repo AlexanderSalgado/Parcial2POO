253d183505be28fd671c98a308f08a8fd9b4d372
Rodolfo Salgado <00191617@uca.edu.sv>
2018-06-03 01:43:54 -0600

    Añadir algunas clases y los paquetes principales

---
+++ src/Egipcios/Faraon.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Egipcios;
+
+/**
+ *
+ * @author rodol
+ */
+public class Faraon implements Egipcios {
+
+    /**
+     *
+     */
+    @Override
+    public  crear(){
+        
+    }
+    
+}
